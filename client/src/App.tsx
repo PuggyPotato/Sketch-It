2e68d8ea98bd6aae3d0b37a658c36331cb82a108
PuggyPotato <qingyaurou@gmail.com>
2025-03-01 17:11:57 +0800

    Added 32*32 grid to the screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,21 @@
 
 function App(){
 
-
+ 
 
 
   return(
     <>
+      <div className="flex justify-center h-screen items-center">
+        <h1 className="absolute top-20 text-3xl left-[660px]">Pixel Draw</h1>
+        <div className="grid grid-cols-32 gap-x-0 w-100 h-30 mb-50 mr-20">
+          {Array.from({length:32 * 32}).map((_,i) =>(
+            <button key={i} id={`${i}`} className="border-2 w-3 h-3 mb-0"></button>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
+
+export default App
