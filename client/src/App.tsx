11353491c974d30713e403e855b6185c17cae2e8
PuggyPotato <qingyaurou@gmail.com>
2025-03-03 22:02:09 +0800

    now the button only turn color when clicked, but when choosing another color the old color changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,13 @@ import { useState } from "react"
 
 function App(){
   const [currentColor,setCurrentColor] = useState("")
+  const [activeButton,setActiveButton] = useState(Array(16 * 16).fill(false))
 
-  
+  const toggleColor = (index:number) =>{
+      setActiveButton((prev) =>
+        prev.map((state,i) => (i===index? !state:state))
+      )
+  }
 
   return(
     <>
@@ -18,7 +23,7 @@ function App(){
         </div>
         <div className="grid grid-cols-16 gap-x-0 w-100 h-30 mb-50 mr-20">
           {Array.from({length:16 * 16}).map((_,i) =>(
-            <button key={i} id={`${i}`} className={`border-2 w-6 h-6 mb-0 ${currentColor}`}></button>
+            <button key={i} id={`${i}`} className={`border-2 w-6 h-6 mb-0 ${activeButton[i] ? currentColor : ""} `} onClick={() =>toggleColor(i)}></button>
           ))}
         </div>
       </div>
