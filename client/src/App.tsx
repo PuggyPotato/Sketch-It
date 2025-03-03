5a763b37cfda49be36029c908a2c743e0b14bb5d
PuggyPotato <qingyaurou@gmail.com>
2025-03-03 21:43:44 +0800

    Added ability for button to turn color, todo:make the button only turn color on click, and only that specific button, not all at the same time

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,13 +12,13 @@ function App(){
       <div className="flex justify-center h-screen items-center">
         <h1 className="absolute top-10 text-3xl left-[660px]">Pixel Draw</h1>
         <div className="grid grid-rows absolute top-20 left-140">
-          <button className="size-4 border m-2">Red</button>
-          <button className="size-4 border m-2">Green</button>
-          <button className="size-4 border m-2">Yellow</button>
+          <button className="size-4 border m-2" onClick={() =>setCurrentColor("bg-yellow-300")}>Red</button>
+          <button className="size-4 border m-2" onClick={() =>setCurrentColor("bg-green-300")}>Green</button>
+          <button className="size-4 border m-2" onClick={() =>setCurrentColor("bg-yellow-300")}>Yellow</button>
         </div>
         <div className="grid grid-cols-16 gap-x-0 w-100 h-30 mb-50 mr-20">
           {Array.from({length:16 * 16}).map((_,i) =>(
-            <button key={i} id={`${i}`} className="border-2 w-6 h-6 mb-0"></button>
+            <button key={i} id={`${i}`} className={`border-2 w-6 h-6 mb-0 ${currentColor}`}></button>
           ))}
         </div>
       </div>
